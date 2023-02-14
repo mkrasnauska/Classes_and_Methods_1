4f8165822e81db54465b24dfa89bc42f54dd468d
Margarita <margaritakrasnauska@gmail.com>
2023-02-14 13:13:33 +0000

    Add project files.

---
+++ Classes_and_Methods_1/Maths.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ClassesAndMethodsAssignment1
+{
+    public class Math
+    {
+        public static int MyDivInt(int a, int b)
+        {
+            return a % b;
+        }
+        public static decimal MyDivDec(decimal a, decimal b)
+        {
+            return Convert.ToInt32(a / b);
+        }
+        public static string MyDivString(string a, string b)
+        {
+            return a + b;
+        }
+
+
+
+    }
+}
---
+++ Classes_and_Methods_1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+
+namespace ClassesAndMethodsAssignment1
+{
+    class Program
+    {
+        public static void Main()
+        {
+            int z = Math.MyDivInt(1, 8);
+            Console.WriteLine("1 8  remainder is : " + z);
+
+            decimal c = Math.MyDivDec(70, 3);
+            Console.WriteLine("70/3 dec  division is result is : " + c);
+
+
+            string x = Math.MyDivString("1234", "555");
+            Console.WriteLine("1234 + 555 string is result is : " + Convert.ToInt32(x));
+            Console.Read();
+        }
+    }
+}
